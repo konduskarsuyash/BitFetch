6f3b1726696e0917e95e0ddb9dfa6f7d10b19cfe
konduskarsuyash <suyashkonduskar27@gmail.com>
2025-12-08 10:08:39 +0530

    health check added

---
+++ src/main/java/com/telegram/Bot/HealthController.java
@@ -0,0 +1,15 @@
+package com.telegram.Bot;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+
+}
